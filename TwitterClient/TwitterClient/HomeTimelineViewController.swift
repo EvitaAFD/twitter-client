105385fff273c42cda2568f7a33a66133951f158
EVE DENISON <evedenison@gmail.com>
2017-03-20 15:02:48 -0700

    built table view and table view cells

---
+++ TwitterClient/TwitterClient/HomeTimelineViewController.swift
@@ -0,0 +1,43 @@
+//
+//  HomeTimelineViewController.swift
+//  TwitterClient
+//
+//  Created by Eve Denison on 3/20/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimelineViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        
+        JSONParser.tweetsFrom(data: JSONParser.sampleJSONData) { (success, tweets) in
+            
+            if(success){
+                guard let tweets = tweets else { fatalError("Tweets came back nil") }
+                for tweet in tweets{
+                    print(tweet.text)
+                }
+            }
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Indexpath: \(indexPath.row)"
+        
+        return cell
+    }
+}
