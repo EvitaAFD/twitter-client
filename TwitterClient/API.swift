1d3f57c6cd55ec015aa5b84942fa317b9fc3fa12
EVE DENISON <evedenison@gmail.com>
2017-03-21 11:56:43 -0700

    build in accounts and social framework and api access

---
+++ TwitterClient/API.swift
@@ -0,0 +1,92 @@
+//
+//  API.swift
+//  TwitterClient
+//
+//  Created by Eve Denison on 3/21/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import Foundation
+import Accounts
+import Social
+
+//may need to change to accouts and an accounts array
+typealias AccountCallback = (ACAccount?) -> ()
+typealias UserCallback = (User?) -> ()
+typealias TweeetsCallback = ([Tweet]?) -> ()
+
+class API {
+
+    static let shared = API()
+    
+    var account : ACAccount?
+    
+    private func loign(callback: @escaping AccountCallback) {
+        
+        let accountStore = ACAccountStore()
+        
+        let accountType = accountStore.accountType(withAccountTypeIdentifier:
+            ACAccountTypeIdentifierTwitter)
+    
+        accountStore.requestAccessToAccounts(with: accountType, options: nil) { (success, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                callback(nil)
+                return
+            }
+            
+            if success {
+                if let account = accountStore.accounts(with: accountType).first as? ACAccount {
+                    callback(account)
+                }
+                
+            } else {
+                print("The user did not allow access to their account")
+                callback(nil)
+            }
+        }
+    }
+    
+    private func getOAuthUser(callback: @escaping UserCallback) {
+    
+        let url = URL(string: "https://api.twitter.com/1.1/account/verify_credentials.json")
+        
+        if let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: .GET, url: url, parameters: nil) {
+        
+            request.account = self.account
+            
+            request.perform(handler: { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                    callback(nil)
+                    return
+                }
+                
+                guard let response = response else { callback(nil); return }
+                guard let data = data else { callback(nil); return }
+                
+                switch response.statusCode {
+                    
+                case 200...299:
+                    //refactor into do, try, catch
+                    if let userJSON = try! JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+                    
+                    }
+                default:
+                    print("Error: response came back with status code: \(response.statusCode)")
+                    callback(nil)
+                }
+                
+            })
+        }
+    }
+    
+    private func updateTimeLine(callback: TweeetsCallback) {
+    
+    }
+    
+    func getTweets(callback: TweeetsCallback) {
+    
+    }
+
+}
