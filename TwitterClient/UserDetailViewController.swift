f10ce3f3e77c635bbd994f207136f2daca4e721c
EVE DENISON <evedenison@gmail.com>
2017-03-23 15:26:31 -0700

    Added files for wednesday- lab, decided to switch to Thursday-lab complete

---
+++ TwitterClient/UserDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  UserDetailViewController.swift
+//  TwitterClient
+//
+//  Created by Eve Denison on 3/23/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
